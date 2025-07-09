e3c0fd0dd63796e690582b74685a6348ed44da17
JunSeoChoi <evan7484@gmail.com>
2025-07-09 14:34:29 +0900

    Feat: Input 컴포넌트 3개 구현
    사이즈 지정 인풋, CTA 인풋, PopUp 인풋

--- src/components/input/FreeInput.tsx
+++ src/components/input/FreeInput.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './Input.module.css';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   fontSize?: number; // px 단위
@@ -18,8 +17,10 @@ const Input: React.FC<InputProps> = ({
     height: height ? `${height}px` : undefined,
     maxWidth: maxWidth ? `${maxWidth}px` : undefined,
   };
+  const base =
+    ' w-full max-w-[353px] h-[56px] px-[12px] py-[14px] rounded-[8px] border-0 bg-[#f9f9f9] text-[#8e8e8e] text-[16px] font-400 leading-[140%] box-border placeholder-[#999] focus:outline-none focus:bg-[#eee]';
 
-  return <input className={styles.input} style={inlineStyle} {...props} />;
+  return <input className={base} style={inlineStyle} {...props} />;
 };
 
 export default Input;
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import styles from './Input.module.css';
 
 const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (
   props
 ) => {
-  return <input className={styles.input} {...props} />;
+  const base =
+    ' w-full max-w-[353px] h-[56px] px-[12px] py-[14px] rounded-[8px] border-0 bg-[#f9f9f9] text-[#8e8e8e] text-[16px] font-400 leading-[140%] box-border placeholder-[#999] focus:outline-none focus:bg-[#eee]';
+
+  return <input className={base} {...props} />;
 };
 
 export default Input;
