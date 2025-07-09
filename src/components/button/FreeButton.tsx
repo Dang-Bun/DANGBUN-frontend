60b1868c801bc8557e53992c1ba5d595fdc30f8b
JunSeoChoi <evan7484@gmail.com>
2025-07-09 14:20:02 +0900

    Design: 기존 css tailwindcss로 수정

--- src/components/button/FreeButton.tsx
+++ src/components/button/FreeButton.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './Button.module.css';
 import classNames from 'classnames';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -11,7 +10,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const Button: React.FC<ButtonProps> = ({
-  variant = 'primary',
+  variant = 'blue',
   fontSize,
   height,
   maxWidth,
@@ -23,10 +22,17 @@ const Button: React.FC<ButtonProps> = ({
     height: height ? `${height}px` : undefined,
     maxWidth: maxWidth ? `${maxWidth}px` : undefined,
   };
+  const base =
+    'w-full rounded-[8px] px-[12px] py-[14px] cursor-pointer transition-[background] duration-300 box-border border-0';
+  const variantClass = {
+    blue: 'bg-[#4d83fd] text-[#fff] font-[Pretendard] font-medium leading-[140%]',
+    thickGray: 'bg-[#bdbdbd] text-[#fff]',
+    gray: 'bg-[#f6f6f6] text-[#8e8e8e] border-[1px] border-solid border-[#dedede]',
+  }[variant];
 
   return (
     <button
-      className={classNames(styles.button, styles[variant])}
+      className={classNames(base, variantClass)}
       style={inlineStyle}
       {...props}
     >
