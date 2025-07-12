afa59d9da92b8476574f19ee7e8d262c06596f75
JunSeoChoi <evan7484@gmail.com>
2025-07-12 15:32:19 +0900

    Feat: 매니저/맴버 아이콘 컴포넌트 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import CTAButton from './components/button/CTAButton';
 import FreeButton from './components/button/FreeButton';
 import Input from './components/input/Input';
 import FreeInput from './components/input/FreeInput';
 import PopUpButton from './components/button/PopUpButton';
 import PopUpInput from './components/input/PopUpInput';
+import SelectableRoleCard from './components/place/SelectableRollCard';
 
 function App() {
+  const [selected, setSelected] = useState<'manager' | 'member' | null>(null);
+
   return (
     <>
       <CTAButton>로그인</CTAButton>
@@ -17,6 +21,20 @@ function App() {
       <Input placeholder='이메일을 입력하세요.' />
       <FreeInput placeholder='이메일' maxWidth={74} height={50} fontSize={16} />
       <PopUpInput placeholder='이메일 입력'></PopUpInput>
+      <SelectableRoleCard
+        role='manager'
+        selected={selected === 'manager'}
+        onClick={() =>
+          setSelected((prev) => (prev === 'manager' ? null : 'manager'))
+        }
+      />
+      <SelectableRoleCard
+        role='member'
+        selected={selected === 'member'}
+        onClick={() =>
+          setSelected((prev) => (prev === 'member' ? null : 'member'))
+        }
+      />
     </>
   );
 }
