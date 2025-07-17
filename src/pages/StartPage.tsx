30d13a632d10851429a2a923f39c4dc7e2ebe2c1
JunSeoChoi <evan7484@gmail.com>
2025-07-17 18:31:21 +0900

    Feat: A001 시작 화면 구현

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import icon from '../assets/Icon.svg';
+import icon from '../assets/Icon_reverse.svg';
+import CTAButton from '../components/button/CTAButton';
+import Input from '../components/input/Input';
+import right_chevron from '../assets/checkIcon/right_chevronImg.svg';
 
 const StartPage = () => {
   return (
-    <div>
-      <img src={icon} alt='아이콘' />
+    <div className='w-full min-h-screen'>
+      <div className='flex justify-center pb-[104px] pt-[120px]'>
+        <img src={icon} alt='아이콘' />
+      </div>
+      <div className='w-full flex flex-col items-center gap-[10px]'>
+        <Input placeholder='이메일을 입력하세요.' />
+        <Input placeholder='비밀번호를 입력하세요.' />
+        <CTAButton>로그인</CTAButton>
+        <div className='w-[353px] flex flex-row justify-end -mt-[2.75px] -mb-[2px]'>
+          <div className='text-[14px] font-normal pr-[9px]'>비밀번호 찾기</div>
+          <img src={right_chevron} alt='오른쪽 화살표' />
+        </div>
+        <CTAButton variant='gray'>회원가입</CTAButton>
+      </div>
     </div>
   );
 };
