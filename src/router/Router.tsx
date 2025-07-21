5c98f25197a96ba484b5fd3f227781d43d85a52b
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2025-07-21 16:57:11 +0900

    Feat : D002~D003 초기 구현

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,6 +6,10 @@ import Z_onBoarding from '../pages/Z_onBoarding';
 import StartPage from '../pages/StartPage';
 import CalendarPage from '../pages/CalendarPage';
 import Notification from '../pages/Notification';
+import PlaceMake1 from '../pages/D/PlaceMake1'; // Ensure this import is correct
+
+import SelectableRoleCard from '../components/place/PlaceRollCard'; // Ensure this import is correct
+import PlaceRollCard from '../components/place/PlaceRollCard'; // Ensure this import is correct
 
 const router = createBrowserRouter([
   {
@@ -24,9 +28,22 @@ const router = createBrowserRouter([
     path: '/calendar',
     element: <CalendarPage />,
   },
- {
+  {
     path: '/alarm',
     element: <Notification />,
+  },
+  {
+    path: 'placemake1',
+    element: <PlaceMake1 />, // Ensure PlaceMake1 is imported correctly
+  },
+  {
+    path: 'rollcard',
+    element: (
+      <div>
+        <SelectableRoleCard role='cafe' selected={false} onClick={() => {}} />
+        <PlaceRollCard role='cafe' selected={false} onClick={() => {}} />
+      </div>
+    ),
   },
 ]);
 
