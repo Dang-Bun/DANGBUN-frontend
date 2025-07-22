92e2e3e8f5d596bc9fef17c417e25e0fa2823561
JunSeoChoi <evan7484@gmail.com>
2025-07-22 17:16:09 +0900

    Feat: 회원가입 완료 페이지 구현

--- src/pages/B/JoinComplete.tsx
+++ src/pages/B/JoinComplete.tsx
@@ -4,15 +4,21 @@ import CTAButton from '../../components/button/CTAButton';
 
 const SelectPlace = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center'>
       <div>
-        <div>회원가입 완료</div>
-        <div>이제 당번에서 플레이스를 생성하거나 참여할 수 있어요</div>
+        <div className='w-full mt-[204px] text-[#000] text-center text-[24px] font-[600]'>
+          회원가입 완료 !
+        </div>
+        <div className='mt-[12px] text-gray-6 text-[14px] font-medium'>
+          이제 당번에서 플레이스를 생성하거나 참여할 수 있어요
+        </div>
       </div>
-      <div>
+      <div className='mt-[56px]'>
         <img src={joinComplete} alt='회원가입 완료 표시' />
       </div>
-      <CTAButton>내 플레이스로 이동</CTAButton>
+      <div>
+        <CTAButton>내 플레이스로 이동</CTAButton>
+      </div>
     </div>
   );
 };
