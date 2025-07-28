6c25e8a9fc70ff3e47fb6f0060f8dc3e2eb2bc14
JunSeoChoi <evan7484@gmail.com>
2025-07-28 13:48:55 +0900

    Feat: 플레이스 선택 화면 구현현

--- src/pages/B/MyPlace.tsx
+++ src/pages/B/MyPlace.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import nothingPlace from '../../assets/placeIcon/nothingPlaceImg.svg';
+import threebar from '../../assets/nav/threebar.svg';
+import CTAButton from '../../components/button/CTAButton';
 
 const MyPlace = () => {
-  return <div>내 플레이스</div>;
+  return (
+    <div>
+      <div className='flex w-full h-[50px] items-center mt-[72px] mb-[205px]'>
+        <div className='flex w-full text-[20px] justify-center'>
+          내 플레이스
+        </div>
+        <img src={threebar} alt='목록' />
+      </div>
+      <div className='w-full flex justify-center mb-[229px]'>
+        <img src={nothingPlace} alt='플레이스 없음' />
+      </div>
+      <div className='flex justify-center'>
+        <CTAButton>플레이스 추가</CTAButton>
+      </div>
+    </div>
+  );
 };
 
 export default MyPlace;
