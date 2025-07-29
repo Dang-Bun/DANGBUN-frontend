c7f9f8d23a06973b282d6e286b7977377126958f
JunSeoChoi <evan7484@gmail.com>
2025-07-29 17:36:42 +0900

    Feat: 플레이스 추가 화면 구현

--- src/pages/B/MyPlace.tsx
+++ src/pages/B/MyPlace.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import nothingPlace from '../../assets/placeIcon/nothingPlaceImg.svg';
 import threebar from '../../assets/nav/threebar.svg';
 import CTAButton from '../../components/button/CTAButton';
+import { useNavigate } from 'react-router-dom';
 
 const MyPlace = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/addPlace');
+  };
   return (
     <div>
       <div className='flex w-full h-[50px] items-center mt-[72px] mb-[205px]'>
@@ -16,7 +22,7 @@ const MyPlace = () => {
         <img src={nothingPlace} alt='플레이스 없음' />
       </div>
       <div className='flex justify-center'>
-        <CTAButton>플레이스 추가</CTAButton>
+        <CTAButton onClick={handleClick}>플레이스 추가</CTAButton>
       </div>
     </div>
   );
