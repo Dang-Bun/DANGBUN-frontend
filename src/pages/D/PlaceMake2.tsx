ec9deb48b1ffbd1f3a712043a3938755f34bc931
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2025-08-07 00:58:12 +0900

    Fix : 기획 변경 반영

--- src/pages/D/PlaceMake2.tsx
+++ src/pages/D/PlaceMake2.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import CTAButton from '../../components/button/CTAButton';
 import BlueX from '../../assets/placeMake/BlueX.svg';
 import PopUpCard from '../../components/PopUp/PopUpCard';
 import FreeButton from '../../components/button/FreeButton';
+import Header from '../../components/HeaderBar';
 
 import { usePlaceApi } from '../../hooks/usePlaceApi';
 
@@ -11,11 +12,9 @@ const PlaceMake2 = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { placeName, role } = location.state || {};
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
-  const [infoList, setInfoList] = React.useState([
-    { label: '이름', value: '' },
-    { label: '이메일', value: '' },
-  ]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [infoList, setInfoList] = useState([{ label: '이메일', value: '' }]);
+  const [name, setName] = useState('');
 
   const handleNext = async () => {
     if (placeName && role) {
@@ -48,18 +47,35 @@ const PlaceMake2 = () => {
   };
 
   return (
-    <div className='flex flex-col w-full h-screen items-center justify-start'>
+    <div className='flex flex-col w-full h-screen mt-[52px] items-center justify-start'>
+      <Header title='' showBackButton={true}></Header>
       <div className='flex flex-col items-start w-[375px] gap-2 mb-7.75 px-5'>
         <h1 className='text-xl font-normal leading-7'>정보를 작성해주세요.</h1>
         <h2 className='text-neutral-400 text-sm font-normal leading-tight'>
-          멤버가 참여하기 전에 꼭 작성해야 할 목록이에요.
+          플레이스에서 표시될 매니저 정보를 입력해주세요.
+          <br />
+          플레이스에 멤버 참여 시 동일한 목록의 정보를 입력할거에요.
         </h2>
       </div>
       <div className='flex flex-col items-start justify-start gap-3 mb-20.5'>
+        <div className='flex flex-row relative'>
+          <p className='w-24 px-4 py-3.5 text-center text-base font-semibold leading-snug'>
+            이름
+          </p>
+          <input
+            type='text'
+            placeholder='입력'
+            className='w-64 h-14 px-3 py-3.5 bg-stone-50 rounded-lg'
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
         {infoList.map((item, index) => (
           <div key={index} className='flex flex-row relative'>
             <button
-              className={`absolute w-4 h-4 bg-neutral-100 rounded-full flex justify-center items-center cursor-pointer ${item.label === '이름' ? 'hidden' : ''}`}
+              className={`absolute w-4 h-4 bg-neutral-100 rounded-full flex justify-center items-center cursor-pointer`}
               onClick={() => {
                 const newList = [...infoList];
                 newList.splice(index, 1);
@@ -73,7 +89,7 @@ const PlaceMake2 = () => {
             </p>
             <input
               type='text'
-              placeholder='"예시 텍스트를 입력해주세요."'
+              placeholder='입력'
               className='w-64 h-14 px-3 py-3.5 bg-stone-50 rounded-lg'
               value={item.value}
               onChange={(e) => {
