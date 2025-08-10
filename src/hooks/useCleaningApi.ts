d96277b3f881ba5088c959077e1f266d9060a49f
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2025-08-10 16:31:21 +0900

    Feat : useCLeaningApi 기본 세팅

--- src/hooks/useCleaningApi.ts
+++ src/hooks/useCleaningApi.ts
@@ -2,8 +2,37 @@ import api from '../apis/axios';
 
 export const useCleaningApi = {
   searchDuty: () => api.get('/cleanings/duties'),
-  //   createPlace: (data: createPlaceRequest) =>
-  //     api.post<defaultApiResponse>('/places', data),
+
+  editCleaning: (id: number) => api.put(`/cleanings/${id}`),
+  deleteCleaning: (id: number) => api.delete(`/cleanings/${id}`),
+  makeCleaning: (data: {
+    placeId: number;
+    cleaningName: string;
+    dutyName: string;
+    members: string[];
+    needPhto: boolean;
+    repeatType: string;
+    repeatDays: string[];
+    detailDates: string[];
+  }) => api.post('/cleanings', data),
+  duty_filter_members: (dutyId: number, memberIds: number[]) =>
+    api.get(`/duties/${dutyId}/cleanings/filter-by-members`, {
+      params: {
+        memberIds: memberIds,
+      },
+    }),
+  cleaningUnAssigned: (placeId: number) =>
+    api.get('/cleanings/unassigned', {
+      params: {
+        placeId: placeId,
+      },
+    }),
+  cleaningsDuty: (memberIds: number[]) =>
+    api.get('cleanings/duties', {
+      params: {
+        memberIds: memberIds,
+      },
+    }),
 };
 
 export default useCleaningApi;
