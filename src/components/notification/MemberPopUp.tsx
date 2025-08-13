32e343d243926aabeb57a2b8325f4ded73991d91
sangh2ee <sangh2ee10@gmail.com>
2025-08-14 01:12:31 +0900

    Design : MemberPopUP 수정

--- src/components/notification/MemberPopUp.tsx
+++ src/components/notification/MemberPopUp.tsx
@@ -9,14 +9,14 @@ const MemberPopUp = ({ onSelect }: MemberPopUpProps) => {
     <div className='w-[120px] h-[80px] bg-[#F9F9F9] flex flex-col items-center justify-center rounded-[8px] shadow-xl space-y-2'>
       <button
         onClick={() => onSelect('dangbun')}
-        className='w-full text-center hover:bg-gray-100'
+        className='w-full text-center hover:bg-[#797C82] text-[#797C82]'
       >
         당번
       </button>
-      <div className='w-[60%] h-px bg-gray-300' />
+      <div className='w-[60%] h-px bg-[#DEDEDE]' />
       <button
         onClick={() => onSelect('member')}
-        className='w-full text-center hover:bg-gray-100'
+        className='w-full text-center text-[#797C82] hover:bg-[#797C82]'
       >
         멤버
       </button>
