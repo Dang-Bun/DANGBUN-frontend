333c37049bd01156a9e68422a76c8a1055b2555e
sangh2ee <sangh2ee10@gmail.com>
2025-08-20 19:22:17 +0900

    Desing : 당번 프로그래스 카드 글자수 제한

--- src/components/home/BlueChip.tsx
+++ src/components/home/BlueChip.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 interface BlueChipProps {
     title : string;
+    maxLength?: number;
 }
-const BlueChip = ({ title }: BlueChipProps) => {
+const BlueChip = ({ title, maxLength = 7 }: BlueChipProps) => {
+  const displayTitle = title.length > maxLength 
+    ? title.slice(0, maxLength)
+    : title;
+
   return (
     <div className='bg-[#4D83FD] rounded-[21px] w-fit h-[28px] px-3 py-2.5 flex items-center'>
-      <span className='text-[14px] text-white font-semibold'>{title}</span>
+      <span className='text-[14px] text-white font-semibold'>{displayTitle}</span>
     </div>
-
   )
 }
 
