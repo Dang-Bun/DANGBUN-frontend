77ce234e8c5219fb7208e94c69ce864c73e546db
JunSeoChoi <evan7484@gmail.com>
2025-08-21 01:57:30 +0900

    Fix: 라우터 로그인 시에만 가능하게 변경

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react';
+import RequireAuth from '../guards/RequireAuth';
 
 import SplashScreen from '../pages/Z/SplashScreen';
 import Z_onBoarding from '../pages/Z/Z_onBoarding';
@@ -51,202 +52,80 @@ import ManagerOverview from '../pages/F/ManagerOverview';
 import CalendarDetail from '../pages/H/CalendarDetail';
 
 const router = createBrowserRouter([
+  // === 공개 라우트 ===
+  { path: '/', element: <SplashScreen /> },
+  { path: '/onboarding', element: <Z_onBoarding /> },
+  { path: '/login', element: <LogIn /> },
+  { path: '/findPassWord', element: <FindPassword /> },
+  { path: '/join', element: <Join /> },
+  { path: '/joinComplete', element: <JoinComplete /> },
+
+  // === 로그인 필요(여기 아래 전부 보호됨) ===
   {
-    path: '/',
-    element: <SplashScreen />,
+    element: <RequireAuth />,
+    children: [
+      { path: '/myPlace', element: <MyPlace /> },
+      { path: '/addPlace', element: <AddPlace /> },
+      { path: '/myInfo', element: <MyInfo /> },
+      { path: '/calendar', element: <CalendarPage /> },
+      { path: '/calendar/:id', element: <CalendarDetail /> },
+      { path: '/home', element: <Home /> },
+      { path: '/home/manager', element: <ManagerHome /> },
+      { path: '/home/member', element: <MemberHome /> },
+      { path: '/home/manager/overview', element: <ManagerOverview /> },
+
+      // 알림(로그인만 필요)
+      { path: '/:placeId/alarm', element: <Notification /> },
+      { path: '/:placeId/alarm/:id', element: <NotificationDetail /> },
+      { path: '/:placeId/alarm/create', element: <NotificationCreate /> },
+
+      // 공용(로그인만)
+      { path: '/memberlist', element: <MemberList /> },
+      { path: '/memberlistmember', element: <MemberList_Member /> },
+      { path: '/memberconfirm', element: <MemberConfirm /> },
+      { path: '/memberlist/entercode', element: <EnterCode /> },
+      { path: '/managerinfo', element: <ManagerInfo /> },
+      { path: '/managerinfomember', element: <ManagerInfo_Member /> },
+
+      //플레이스 생성
+      { path: '/placemake1', element: <PlaceMake1 /> },
+      { path: '/placemake2', element: <PlaceMake2 /> },
+      { path: '/placemake3', element: <PlaceMake3 /> },
+      { path: '/PlaceJoin1', element: <PlaceJoin1 /> },
+      { path: '/PlaceJoin2', element: <PlaceJoin2 /> },
+
+      // 청소(로그인만)
+      { path: '/cleanuplist', element: <CleanUpList /> },
+      { path: '/cleanuplistmember', element: <CleanUpList_Member /> },
+      { path: '/cleanadd', element: <CleanAdd /> },
+      { path: '/cleanedit', element: <CleanEdit /> },
+      { path: '/undangbun', element: <UnDangbun /> },
+      { path: '/undangbunmember', element: <UnDangbun_Member /> },
+      { path: '/cleaninfo', element: <CleanInfo data={undefined} /> },
+      { path: '/placedetailed', element: <PlaceDetailed /> },
+
+      // === 매니저 전용(여기 아래 전부 매니저만) ===
+
+      { path: '/setting/manager', element: <SettingManager /> },
+      { path: '/management/manager', element: <ManagementManager /> },
+      { path: '/management/manager/create', element: <CreateDangbun /> },
+      { path: '/management/manager/duty', element: <DutyManagement /> },
+      { path: '/management/manager/duty/modify', element: <ModifyDuty /> },
+      { path: '/management/manager/duty/addclean', element: <AddClean /> },
+      { path: '/dangerzone/manager', element: <DangerZoneManager /> },
+
+      // === 멤버 전용(필요하면) ===
+
+      { path: '/setting/member', element: <SettingMember /> },
+      { path: '/management/member', element: <ManagementMember /> },
+      { path: '/management/member/duty', element: <DutyMember /> },
+      { path: '/dangerzone/member', element: <DangerZoneMember /> },
+    ],
   },
-  {
-    path: '/onboarding',
-    element: <Z_onBoarding />,
-  },
-  {
-    path: '/login',
-    element: <LogIn />,
-  },
-  {
-    path: '/findPassWord',
-    element: <FindPassword />,
-  },
-  {
-    path: '/join',
-    element: <Join />,
-  },
-  {
-    path: '/joinComplete',
-    element: <JoinComplete />,
-  },
-  {
-    path: '/myPlace',
-    element: <MyPlace />,
-  },
-  {
-    path: '/addPlace',
-    element: <AddPlace />,
-  },
-  {
-    path: '/myInfo',
-    element: <MyInfo />,
-  },
-  {
-    path: '/calendar',
-    element: <CalendarPage />,
-  },
-  {
-    path: '/calendar/:id',
-    element: <CalendarDetail />,
-  },
-  {
-    path: '/home',
-    element: <Home />,
-  },
-  {
-    path: '/home/manager',
-    element: <ManagerHome />,
-  },
-  {
-    path: '/home/manager/overview',
-    element: <ManagerOverview />,
-  },
-  {
-    path: '/home/member',
-    element: <MemberHome />,
-  },
-  {
-    path: '/:placeId/alarm',
-    element: <Notification />,
-  },
-  {
-    path: '/:placeId/alarm/:id',
-    element: <NotificationDetail />,
-  },
-  {
-    path: '/:placeId/alarm/create',
-    element: <NotificationCreate />,
-  },
-  {
-    path: 'placemake1',
-    element: <PlaceMake1 />,
-  },
-  {
-    path: 'placemake2',
-    element: <PlaceMake2 />,
-  },
-  {
-    path: 'placemake3',
-    element: <PlaceMake3 />,
-  },
-  {
-    path: 'PlaceJoin1',
-    element: <PlaceJoin1 />,
-  },
-  {
-    path: 'PlaceJoin2',
-    element: <PlaceJoin2 />,
-  },
-  {
-    path: '/setting/manager',
-    element: <SettingManager />,
-  },
-  {
-    path: '/setting/member',
-    element: <SettingMember />,
-  },
-  {
-    path: '/management/manager',
-    element: <ManagementManager />,
-  },
-  {
-    path: '/management/member',
-    element: <ManagementMember />,
-  },
-  {
-    path: '/management/member/duty',
-    element: <DutyMember />,
-  },
-  {
-    path: '/management/manager/create',
-    element: <CreateDangbun />,
-  },
-  {
-    path: '/management/manager/duty',
-    element: <DutyManagement />,
-  },
-  {
-    path: '/management/manager/duty/modify',
-    element: <ModifyDuty />,
-  },
-  {
-    path: '/management/manager/duty/addclean',
-    element: <AddClean />,
-  },
-  {
-    path: '/cleanuplist',
-    element: <CleanUpList />,
-  },
-  {
-    path: '/cleanuplistmember',
-    element: <CleanUpList_Member />,
-  },
-  {
-    path: '/cleanadd',
-    element: <CleanAdd />,
-  },
-  {
-    path: '/cleanedit',
-    element: <CleanEdit />,
-  },
-  {
-    path: '/undangbun',
-    element: <UnDangbun />,
-  },
-  {
-    path: '/undangbunmember',
-    element: <UnDangbun_Member />,
-  },
-  {
-    path: '/cleaninfo',
-    element: <CleanInfo data={undefined} />,
-  },
-  {
-    path: '/placedetailed',
-    element: <PlaceDetailed />,
-  },
-  {
-    path: '/dangerzone/manager',
-    element: <DangerZoneManager />,
-  },
-  {
-    path: '/dangerzone/member',
-    element: <DangerZoneMember />,
-  },
-  {
-    path: '/memberlist',
-    element: <MemberList />,
-  },
-  {
-    path: '/memberlistmember',
-    element: <MemberList_Member />,
-  },
-  {
-    path: '/memberconfirm',
-    element: <MemberConfirm />,
-  },
-  {
-    path: '/managerinfo',
-    element: <ManagerInfo />,
-  },
-  {
-    path: '/managerinfomember',
-    element: <ManagerInfo_Member />,
-  },
-  {
-    path: '/memberlist/entercode',
-    element: <EnterCode />,
-  },
+
+  // 404
+  { path: '*', element: <div>Not Found</div> },
 ]);
 
-const Router = () => {
-  return <RouterProvider router={router} />;
-};
-
-export default Router;
+const AppRouter = () => <RouterProvider router={router} />;
+export default AppRouter;
